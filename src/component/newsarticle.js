b6aa8aa4b6a3703bfb7c68e6fcaf0f496b4e3037
RengalakshmiBalasubramanian <rengalakshmib09@gmail.com>
2022-06-28 23:32:27 -0400

    news

---
+++ src/component/newsarticle.js
@@ -0,0 +1,12 @@
+function News(){
+    return(
+        <div className = "News">
+            <h1>NASA’s CAPSTONE Mission Launches to the Moon</h1>
+            <p>The small spacecraft will be the first to head to the moon in a year when other planned missions have yet to get off the launchpad.
+            NASA's CAPSTONE mission has launched to the moon.The small 59-foot-tall spacecraft carrying capstone lifted off from a launchpad along the eastern coast of New Zealand lifted shortly before 6 a.m. Eastern. It is the first to head to the moon in a year. The spacecraft will study a specific orbit where NASA plans to build a small space station for astronauts to stop at before and after going to the moon's surface.
+            </p>
+        </div>
+    )
+
+}
+export default News;
---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import News from "../component/newsarticle.js";
+
+
+class Home extends React.Component{
+    render(){
+        return(
+        <div>
+            <News />
+        </div>
+        )
+    }
+}
+export default Home;
